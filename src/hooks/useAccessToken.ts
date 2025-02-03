2d1fe47e3bca0cf78922b30a451ac89fbeab6ace
sina rahimi <sina.1999.rahimi@gamil.com>
2025-02-03 11:50:23 +0330

    viewing tokens in the website

---
+++ src/hooks/useAccessToken.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+
+const useAccessToken = () => {
+  const [accessToken, setAccessToken] = useState(Cookies.get("access") || "");
+  const [refreshToken, setRefreshToken] = useState(
+    Cookies.get("refresh") || ""
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newAccessToken = Cookies.get("access");
+      const newRefreshToken = Cookies.get("refresh");
+      if (newAccessToken !== accessToken) {
+        setAccessToken(newAccessToken || "");
+      }
+      if (newRefreshToken !== refreshToken)
+        setRefreshToken(newRefreshToken || "");
+    }, 1000); // Check every second (adjust as needed)
+
+    return () => clearInterval(interval);
+  }, [accessToken, refreshToken]);
+
+  return {
+    access: accessToken,
+    refreshToken: refreshToken,
+  };
+};
+
+export default useAccessToken;

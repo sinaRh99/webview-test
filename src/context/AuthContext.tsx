f727868ee4dea082cd55a9d837ed7a3d9928e65d
seeno <sina.leagueoflegends@gmail.com>
2025-02-04 19:54:09 +0330

    changed get cookie logic

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import {
   createContext,
@@ -6,8 +6,8 @@ import {
   useEffect,
   ReactNode,
   useContext,
-} from "react";
-import Cookies from "js-cookie";
+} from 'react';
+import Cookies from 'js-cookie';
 
 interface AuthContextType {
   token: string | null;
@@ -19,23 +19,29 @@ interface AuthContextType {
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [token, setToken] = useState<string | null>(null);
+  const [token, setToken] = useState<string>('');
 
   useEffect(() => {
-    const storedToken = Cookies.get("access");
-    if (storedToken) setToken(storedToken);
-  }, []);
+    const interval = setInterval(() => {
+      const newAccessToken = Cookies.get('access');
+      if (newAccessToken !== token) {
+        setToken(newAccessToken || '');
+      }
+    }, 1000); // Che
+
+    return () => clearInterval(interval);
+  }, [token]);
 
   const login = (newToken?: string) => {
     if (newToken) {
       setToken(newToken);
-      Cookies.set("access", newToken);
+      Cookies.set('access', newToken);
     } else logout();
   };
 
   const logout = () => {
-    Cookies.remove("access");
-    setToken(null);
+    Cookies.remove('access');
+    setToken('');
   };
 
   return (
@@ -49,6 +55,6 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
-  if (!context) throw new Error("useAuth must be used within an AuthProvider");
+  if (!context) throw new Error('useAuth must be used within an AuthProvider');
   return context;
 };
